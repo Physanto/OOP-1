ef03a7547d61895b1ce6c79a8496d9b7ca29b656
Physanto <hackmanu6@gmail.com>
2024-10-13 20:30:49 -0500

    ...

---
+++ Manuel-Figueroa-Taller-2d-Corte/Maestro.java
@@ -0,0 +1,47 @@
+/*
+ * author Manuel Figueroa
+ */
+
+public class Maestro{
+
+    private String id;
+    private String nombre;
+    private Strin cel;
+
+    public Maestro(){
+
+    }
+
+    public Maestro(Strin id, String nombre, String cel){
+
+        this.id = id;
+        this.nombre = nombre;
+        this.cel = cel;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre
+    }
+
+    public String getCel(){
+        return cel;
+    }
+
+    public void setCel(String cel){
+        this.cel = cel;
+    }
+
+    public
+}
